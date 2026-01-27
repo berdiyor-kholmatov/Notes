414a26c099fda8161554dbf345332780c82bfad7
Berdiyor Kholmatov <berdiyor.1.29@gmail.com>
2026-01-27 13:51:42 +0500

    The separation border added between two sections list-detail

--- app/src/main/java/com/example/listofnotes/ui/noteList/ListView.kt
+++ app/src/main/java/com/example/listofnotes/ui/noteList/ListView.kt
@@ -1,12 +1,13 @@
 package com.example.listofnotes.ui.noteList
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -17,30 +18,20 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.text.ClickableText
+import androidx.compose.foundation.magnifier
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Done
-import androidx.compose.material3.AssistChip
-import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SwipeToDismissBox
-import androidx.compose.material3.SwipeToDismissBoxValue
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
-import androidx.compose.material3.adaptive.ExperimentalMaterial3AdaptiveApi
-import androidx.compose.material3.adaptive.layout.AnimatedPane
-import androidx.compose.material3.adaptive.layout.ListDetailPaneScaffoldRole
-import androidx.compose.material3.adaptive.navigation.NavigableListDetailPaneScaffold
-import androidx.compose.material3.adaptive.navigation.rememberListDetailPaneScaffoldNavigator
 import androidx.compose.material3.rememberSwipeToDismissBoxState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -48,146 +39,98 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.listofnotes.R
-import com.example.listofnotes.data.db.MyDatabase
-import com.example.listofnotes.data.db.NoteEntity
 import com.example.listofnotes.domain.model.NoteTitle
-import com.example.listofnotes.ui.theme.outlineDark
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlin.random.Random
 import androidx.compose.material3.SwipeToDismissBoxValue.StartToEnd
 import androidx.compose.material3.SwipeToDismissBoxValue.EndToStart
 import androidx.compose.material3.SwipeToDismissBoxValue.Settled
-import androidx.compose.ui.draw.drawBehind
+import androidx.compose.material3.VerticalDivider
 import androidx.compose.ui.graphics.lerp
-import com.example.listofnotes.ui.experementalSwipes.TodoItem
+
 
 
 @Composable
 fun NotesListView(state: ListViewState, onEvent: (ListEvent) -> Unit, onNoteClick: (NoteTitle) -> Unit, addButtonClicked: () -> Unit) {
 
-
-    LazyColumn(
-        modifier = Modifier.systemBarsPadding()
+    Row(
+        modifier = Modifier.systemBarsPadding().fillMaxSize()
     ) {
+        LazyColumn(
+            modifier = Modifier.weight(1f),
+        ) {
 
-        stickyHeader {
-            Row(modifier = Modifier
-                .background(MaterialTheme.colorScheme.primaryContainer),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.Center
-            ) {
-                IconButton(
-                    modifier = Modifier
-                        .size(56.dp)
-                        .padding(16.dp),
-                    onClick = {  }
+            stickyHeader {
+                Row(modifier = Modifier
+                    .background(MaterialTheme.colorScheme.primaryContainer),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center
                 ) {
-                    Icon(
-                        modifier = Modifier.size(24.dp),
-                        painter = painterResource(id = R.drawable.menu_24),
-                        contentDescription = null,
-                        tint =  MaterialTheme.colorScheme.onPrimaryContainer,
+                    IconButton(
+                        modifier = Modifier
+                            .size(56.dp)
+                            .padding(16.dp),
+                        onClick = {  }
+                    ) {
+                        Icon(
+                            modifier = Modifier.size(24.dp),
+                            painter = painterResource(id = R.drawable.menu_24),
+                            contentDescription = null,
+                            tint =  MaterialTheme.colorScheme.onPrimaryContainer,
+                        )
+                    }
+
+                    Spacer(modifier = Modifier.weight(1f))
+
+                    Text(text = "Notes", modifier = Modifier,
+                        color = MaterialTheme.colorScheme.onPrimaryContainer,
+                        fontSize = 24.sp
                     )
+
+                    Spacer(modifier = Modifier.weight(1f))
+
+                    IconButton(
+                        modifier = Modifier
+                            .size(56.dp)
+                            .padding(16.dp),
+                        onClick = addButtonClicked
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.outline_add_circle_24),
+                            contentDescription = null,
+                            tint =  MaterialTheme.colorScheme.onPrimaryContainer,
+                        )
+                    }
+
                 }
+            }
 
-                Spacer(modifier = Modifier.weight(1f))
-
-                Text(text = "Notes", modifier = Modifier,
-                    color = MaterialTheme.colorScheme.onPrimaryContainer,
-                    fontSize = 24.sp
+            items(
+                items = state.notes,
+                key = { it.id }
+            ) { note ->
+                NoteListItem(
+                    noteItem = note,
+                    onNoteItemClick = onNoteClick,
+                    onToggleDone = { onEvent(ListEvent.IsDoneButtonClicked(it.id)) },
+                    onRemove = { onEvent(ListEvent.DeleteButtonClicked(it.id)) },
+                    modifier = Modifier.animateItem()
                 )
-
-                Spacer(modifier = Modifier.weight(1f))
-
-                IconButton(
+                Spacer(
                     modifier = Modifier
-                        .size(56.dp)
-                        .padding(16.dp),
-                    onClick = addButtonClicked
-                ) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.outline_add_circle_24),
-                        contentDescription = null,
-                        tint =  MaterialTheme.colorScheme.onPrimaryContainer,
-                    )
-                }
-
+                        .fillMaxWidth()
+                        .height(1.dp)
+                        .padding(horizontal = 6.dp)
+                        .background(MaterialTheme.colorScheme.onSecondaryContainer.copy(alpha = 0.8f))
+                )
             }
         }
 
-        items(
-            items = state.notes,
-            key = { it.id }
-        ) { note ->
-            NoteListItem(
-                noteItem = note,
-                onNoteItemClick = onNoteClick,
-                onToggleDone = { onEvent(ListEvent.IsDoneButtonClicked(it.id)) },
-                onRemove = { onEvent(ListEvent.DeleteButtonClicked(it.id)) },
-                modifier = Modifier.animateItem()
-            )
-            Spacer(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(1.dp)
-                    .background(MaterialTheme.colorScheme.onSecondaryContainer.copy(alpha = 0.8f))
-            )
-        }
-/*
-//        items(state.notes){ note ->
-//            Row(modifier = Modifier
-//                .background(MaterialTheme.colorScheme.secondaryContainer),
-//                verticalAlignment = Alignment.CenterVertically,
-//                horizontalArrangement = Arrangement.Center
-//            ) {
-//                IconButton(
-//                    modifier = Modifier
-//                        .size(56.dp)
-//                        .padding(16.dp),
-//                    onClick = addButtonClicked
-//                ) {
-//                    Icon(
-//                        painter = painterResource(id =
-//                            if ( note.isDone )
-//                                R.drawable.check_circle_24dp
-//                            else
-//                                R.drawable.circle_24dp),
-//                        contentDescription = null,
-//                        tint = MaterialTheme.colorScheme.onSecondaryContainer,
-//                        modifier = Modifier.size(24.dp)
-//                    )
-//                }
-//                TextButton({ onNoteClick(note) }, content = {Text(text = note.title, fontSize = 20.sp, color = MaterialTheme.colorScheme.onSecondaryContainer)}, modifier = Modifier
-//                    .weight(1f))
-//
-//
-//                IconButton(
-//                    modifier = Modifier
-//                        .size(56.dp)
-//                        .padding(16.dp),
-//                    onClick = addButtonClicked
-//                ) {
-//                    Icon(
-//                        painter = painterResource(id = R.drawable.delete_24dp),
-//                        contentDescription = null,
-//                        tint = MaterialTheme.colorScheme.onSecondaryContainer,
-//                        modifier = Modifier.size(24.dp)
-//
-//                    )
-//                }
-//
-//            }
-//
-//            Spacer(
-//                modifier = Modifier
-//                    .fillMaxWidth()
-//                    .height(1.dp)
-//                    .background(MaterialTheme.colorScheme.onSecondaryContainer.copy(alpha = 0.8f))
-//            )
-//        }
- */
+        Box(
+            modifier = Modifier
+                .fillMaxHeight()
+                //.weight(2f)
+                .width(1.dp)
+                .background(MaterialTheme.colorScheme.onSecondaryContainer.copy(alpha = 0.8f))
+        )
     }
 }
 
@@ -265,145 +208,3 @@ fun NoteListItem(
                 .background(MaterialTheme.colorScheme.secondaryContainer))
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//
-//
-//
-//
-//@OptIn(ExperimentalMaterial3AdaptiveApi::class)
-//@Composable
-//fun ListDetailLayout(modifier: Modifier = Modifier, users: List<NoteEntity>, db: MyDatabase) {
-//    val scaffoldNavigator = rememberListDetailPaneScaffoldNavigator<Any>()
-//    val scope = rememberCoroutineScope()
-//    NavigableListDetailPaneScaffold(
-//        modifier = modifier,
-//        navigator = scaffoldNavigator,
-//        listPane = {
-//            Column(
-//                modifier = Modifier
-//                    .fillMaxSize()
-//                    .systemBarsPadding()
-//            ) {
-//                LazyColumn(
-//                    modifier = Modifier
-//                        .fillMaxWidth()
-//                        .weight(1f),
-//
-//                    ) {
-//                    items(items = users) {
-//                        Text(text = "Name: ${it.text}\n",
-//                            modifier = Modifier
-//                                .fillParentMaxWidth()
-//                                .clickable {
-//                                    scope.launch {
-//                                        scaffoldNavigator.navigateTo(
-//                                            pane = ListDetailPaneScaffoldRole.Detail,
-//                                            contentKey = "ID: ${it.id}\n" +
-//                                                    "Content: ${it.text}\n" +
-//                                                    "Created at: ${it.dateOfCreating}\n" +
-//                                                    "Edited at: ${it.dateOfEditing}\n"
-//                                        )
-//                                    }
-//                                }
-//                                .padding(16.dp)
-//                        )
-//                    }
-//                }
-//
-//                Button(
-//                    modifier = Modifier
-//                        .fillMaxWidth()
-//                        .padding(16.dp),
-//                    onClick = {
-//                        CoroutineScope(Dispatchers.IO).launch {
-//                            db.getMyDao().insert(
-//                                NoteEntity(
-//                                    text = "Name ${Random.nextInt()}",
-//                                    dateOfCreating = "${Random.nextInt()}",
-//                                    dateOfEditing = "${Random.nextInt()}",
-//                                    isDone = false,
-//                                    title = ""
-//                                )
-//                            )
-//                        }
-//                    }
-//                ) {
-//                    Text(text = "Add random name")
-//                }
-//            }
-//
-//        },
-//        detailPane = {
-//            val content = scaffoldNavigator.currentDestination?.contentKey?.toString() ?: "Select an item"
-//            AnimatedPane {
-//                Column(
-//                    modifier = Modifier
-//                        .fillMaxSize()
-//                        .background(MaterialTheme.colorScheme.primaryContainer),
-//                    horizontalAlignment = Alignment.CenterHorizontally,
-//                    verticalArrangement = Arrangement.Center,
-//                ){
-//                    Text(content)
-//                    Row{
-//                        AssistChip(
-//                            onClick = {
-//                                scope.launch {
-//                                    scaffoldNavigator.navigateTo(
-//                                        pane = ListDetailPaneScaffoldRole.Extra,
-//                                        contentKey = "Option 1"
-//                                    )
-//                                }
-//                            },
-//                            label = {
-//                                Text("Option 1")
-//                            }
-//                        )
-//                        Spacer(modifier = Modifier.width(16.dp))
-//                        AssistChip(
-//                            onClick = {
-//                                scope.launch {
-//                                    scaffoldNavigator.navigateTo(
-//                                        pane = ListDetailPaneScaffoldRole.Extra,
-//                                        contentKey = "Option 2"
-//                                    )
-//                                }
-//                            },
-//                            label = {
-//                                Text("Option 2")
-//                            }
-//                        )
-//                    }
-//
-//                }
-//            }
-//        },
-//        extraPane = {
-//            val content = scaffoldNavigator.currentDestination?.contentKey?.toString() ?: "Select an item"
-//            AnimatedPane {
-//                Box(
-//                    modifier = Modifier
-//                        .fillMaxSize()
-//                        .background(MaterialTheme.colorScheme.secondaryContainer),
-//                    contentAlignment = Alignment.Center,
-//                ) {
-//                    Text(content)
-//                }
-//            }
-//        }
-//    )
-//}
